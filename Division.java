cfe6783f4b7a23bb89e36d75fdcd5227b4cde436
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-08-04 21:01:30 +0530

    import java.util.Scanner; public class add2Numbers {     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a,b,c;         a = sc.nextInt();         b = sc.nextInt();         c = a / b;         System.out.println(c);     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class add2Numbers {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a / b;
+        System.out.println(c);
+    }
+}
