94c41059ae79c4c997e5274aa6249c720371a7f6
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-08-05 09:39:36 +0530

    import java.util.Scanner; public class maxnohs{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a,b;         a = sc.nextInt();         b = (a*(a-1))/2;         System.out.println(b);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class maxnohs{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = (a*(a-1))/2;
+        System.out.println(b);
+    }
+}
