54f2d350665d1e61233a57dda8c38bd6f22358f2
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-08-05 19:07:30 +0530

    import java.util.*; public class pandemic{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int i;         i = sc.nextInt();         if(i==2){             System.out.printf("1");         }         else if(i==1){             System.out.printf("0");         }         else{             System.out.printf("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pandemic{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i;
+        i = sc.nextInt();
+        if(i==2){
+            System.out.printf("1");
+        }
+        else if(i==1){
+            System.out.printf("0");
+        }
+        else{
+            System.out.printf("2");
+        }
+    }
+}
