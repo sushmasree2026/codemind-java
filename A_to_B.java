e65a1ff6b4d09e05dcbe03be883b1cc2e9317823
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-08-19 19:31:37 +0530

    import java.util.*; public class a_to_b{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a,b;         a = sc.nextInt();         b = sc.nextInt();         for(int i=a;i<=b;i++){             System.out.print(i+" ");         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class a_to_b{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        for(int i=a;i<=b;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
