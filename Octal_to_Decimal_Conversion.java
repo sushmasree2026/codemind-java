d31ad16f2c9a117d03462b5ab0116c08125017b5
Sushma Sree <119730289+sushmasree2026@users.noreply.github.com>
2023-08-25 10:10:39 +0530

    import java.util.*; public class oct_to_dec{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         System.out.println(Integer.parseInt(a,8));     }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class oct_to_dec{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(Integer.parseInt(a,8));
+    }
